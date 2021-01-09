1af4f6a00972f80821a607610db16fbfaabcd708
Austin WIley <wileybeer@gmail.com>
2021-01-09 09:40:03 -0700

    commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 8080
+// console.log(process.env.PORT)
+// console.log(PORT)
 
 app.get('/', function(req, res) {
     res.render('index.ejs');
